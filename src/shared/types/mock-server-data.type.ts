5ca82c120eedfd801f9bac57a25dc4d118bb4769
alekseib <aleksei.b@adngames.com>
2023-10-03 21:58:42 +0300

    update workflow, add service, json-server, connect it

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,11 @@
+export type MockServerData = {
+  titles: string[];
+  type: string[];
+  descriptions: string[];
+  offerImages: string[];
+  categoryImages: string[];
+  city: string[];
+  users: string[];
+  emails: string[];
+  avatars: string[];
+};
